b962c87cdd90b303e914203d47a0e858aa82fe9b
Brian Cerney <brianecerney@gmail.com>
2019-11-14 00:12:53 +0000

    User login, profiles, error handling, email support, Bootstrap facelift.

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,62 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler, SMTPHandler
+
 from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import routes, models
+login = LoginManager(app)
+login.login_view = 'login'
+
+mail = Mail(app)
+
+bootstrap = Bootstrap(app)
+
+from app import errors, models, routes
+
+if not app.debug:
+    # Email error logging
+    if app.config['MAIL_SERVER']:
+        # auth = None
+        # if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+        #     auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        # secure = None
+        # if app.config['MAIL_USE_TLS']:
+        #     secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='gtRPG Failure',
+            )
+            # credentials=auth, secure=secure
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    # File error logging
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/gtRPG.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('gtRPG startup')
+
 
 if __name__ == '__main__':
     app.run()
